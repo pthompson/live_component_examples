41fbca266b3e0c86fa5483b3a0c973346f7b3c41
Patrick Thompson <patrick@inkstonesoftware.com>
2020-06-29 17:35:12 -0700

    Change live_link calls to live_patch.

--- lib/demo_web/live/example_live/example_live.ex
+++ lib/demo_web/live/example_live/example_live.ex
@@ -7,13 +7,13 @@ defmodule DemoWeb.LiveComponentExamples do
       <article class="column">
         <h4>Simple LiveComponent Examples</h4>
         <ul>
-          <li><%= live_link("Static Title Example", to: Routes.live_path(@socket, DemoWeb.StaticTitleLiveView)) %></li>
-          <li><%= live_link("Assigns Title Example", to: Routes.live_path(@socket, DemoWeb.AssignsTitleLiveView)) %></li>
-          <li><%= live_link("Stateless Component Example", to: Routes.live_path(@socket, DemoWeb.StatelessComponentLiveView)) %></li>
-          <li><%= live_link("Stateful Component Example", to: Routes.live_path(@socket, DemoWeb.StatefulComponentLiveView)) %></li>
-          <li><%= live_link("Stateful Send Self Component Example", to: Routes.live_path(@socket, DemoWeb.StatefulSendSelfComponentLiveView)) %></li>
-          <li><%= live_link("Stateful Preload Component Example", to: Routes.live_path(@socket, DemoWeb.StatefulPreloadComponentLiveView)) %></li>
-          <li><%= live_link("Component Blocks Example", to: Routes.live_path(@socket, DemoWeb.ComponentBlocksLiveView)) %></li>
+          <li><%= live_patch("Static Title Example", to: Routes.live_path(@socket, DemoWeb.StaticTitleLiveView)) %></li>
+          <li><%= live_patch("Assigns Title Example", to: Routes.live_path(@socket, DemoWeb.AssignsTitleLiveView)) %></li>
+          <li><%= live_patch("Stateless Component Example", to: Routes.live_path(@socket, DemoWeb.StatelessComponentLiveView)) %></li>
+          <li><%= live_patch("Stateful Component Example", to: Routes.live_path(@socket, DemoWeb.StatefulComponentLiveView)) %></li>
+          <li><%= live_patch("Stateful Send Self Component Example", to: Routes.live_path(@socket, DemoWeb.StatefulSendSelfComponentLiveView)) %></li>
+          <li><%= live_patch("Stateful Preload Component Example", to: Routes.live_path(@socket, DemoWeb.StatefulPreloadComponentLiveView)) %></li>
+          <li><%= live_patch("Component Blocks Example", to: Routes.live_path(@socket, DemoWeb.ComponentBlocksLiveView)) %></li>
         </ul>
       </article>
     </section>
